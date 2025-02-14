58d97088a2b5b672f393264c21748466b0988c92
Mornac <imornac@gmail.com>
2025-02-14 15:53:26 +0100

    Cprogram_prints_sentence_&nwline_gcc -Wall_option

---
+++ hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ *main - prints a character
+ *
+ *Return: 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
