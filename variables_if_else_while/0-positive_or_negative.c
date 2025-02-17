ff41d540aa353b76f686ac1455207b8910827e5c
Mornac <imornac@gmail.com>
2025-02-17 09:57:20 +0100

    assign_randomnumber_to_variablen_eachtime_it_executed

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-/* more headers goes there */
 
-/* betty style doc for function main goes there */
 /**
  * main - Entry point
- *
- * return: Always 0 (Success)
+ * that only test
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
