8aa0f392baaca21529d2a33ec81d10b6892e4171
Mornac <imornac@gmail.com>
2025-02-17 17:16:48 +0100

    print_number_0_9

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * that only test
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char n;
+
+	for (n = 0; n < 10; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
