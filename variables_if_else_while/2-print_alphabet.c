42b34c3902b498805686a6f4c44ba91a1bb0e0a7
Mornac <imornac@gmail.com>
2025-02-17 12:06:58 +0100

    2print_alphabet_lowercase

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ * that only test
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	for (int i = 'a'; i <= 'z' ; i++)
+	{
+		putchar(i);
+		putchar(' ');
+	}
+	return (0);
+}
