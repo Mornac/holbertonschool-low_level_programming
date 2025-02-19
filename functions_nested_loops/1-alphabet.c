f3de6d6150172d2677778ab083ba1992af7ee5b0
Mornac <imornac@gmail.com>
2025-02-19 12:20:10 +0100

    prints the alphabet, in lowercase, followed by a new line

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -5,9 +5,13 @@
  *
  * Return: Always 0.
  */
-int main(void)
+void print_alphabet(void)
 {
-	print_alphabet();
+	char ch = 'a';
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		_putchar(ch);
+	}
 	_putchar('\n');
-	return (0);
 }
