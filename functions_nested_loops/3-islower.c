cb478495070e4b90b7be9fbab036b8b7cc575cff
Mornac <imornac@gmail.com>
2025-02-19 15:35:04 +0100

    function that checks for lowercase character

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * int _islower(int c) - function
+ *
+ * Return: Always 0.
+ */
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+	{
+		_putchar('1');
+	}
+	else
+	{
+		_putchar('0');
+	}
+	return (0);
+}
