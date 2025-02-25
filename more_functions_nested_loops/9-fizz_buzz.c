dddd133212c0bb61bda5576dc461ab02d880c286
Mornac <imornac@gmail.com>
2025-02-25 12:35:53 +0100

    program that prints the numbers from 1 to 100

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include "stdio.h"
+/**
+ * main - Entry point
+ * that only test
+ * Return: Always 0
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 1; n <= 100; n++)
+	{
+		if (n % 3 == 0 && n % 5 == 0)
+		{
+			printf("FizzBuzz");
+		}
+		else if (n % 3 == 0)
+		{
+			printf("Fizz");
+		}
+		else if (n % 5 == 0)
+		{
+			printf("Buzz");
+		}
+		else
+		{
+			printf("%d", n);
+		}
+			putchar (' ');
+	}
+	putchar ('\n');
+	return (0);
+}
