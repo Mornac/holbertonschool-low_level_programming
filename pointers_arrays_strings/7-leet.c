3b95ff238a6d5022bd614103d0cbd99fcd98c723
Mornac <imornac@gmail.com>
2025-03-02 14:46:31 +0100

    function that encodes a string into 1337

---
+++ pointers_arrays_strings/7-leet.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+#include "2-strlen.c"
+
+/**
+ *leet - function that encodes a string into 1337
+ *@s: capitalize words of string
+ *Return: changed string
+ */
+char *leet(char *s)
+{
+	char a[] = {'a', 'e', 'o', 't', 'l' };
+	char b[] = {'A', 'E', 'O', 'T', 'L' };
+	char n[] = {4, 3, 0, 7, 1 };
+	int i;
+
+	for (i = 0; i < 5; i++)
+	{
+		if (*s == a[i] || *s == b[i])
+			*s = n[i];
+	}
+	s++;
+
+	return (s);
+}
