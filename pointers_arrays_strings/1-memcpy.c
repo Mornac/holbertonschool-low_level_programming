30ddd33f291f3304915253fb4542441359014620
Mornac <imornac@gmail.com>
2025-03-03 11:25:27 +0100

    function that copies memory area

--- pointers_arrays_strings/1-memcpy.c
+++ pointers_arrays_strings/1-memcpy.c
@@ -18,7 +18,7 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 	for (i = 0; i < n ; i++)
 	{
 		dest[i] = *src;
-		*dest = strlen(src) + 1;
+		*dest = _strlen(src) + 1;
 	}
 
 	return (dest);
