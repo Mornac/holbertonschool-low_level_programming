7b5607e965f6f0869a7a49e87f1f5fc21952d3d2
Mornac <imornac@gmail.com>
2025-03-03 16:41:40 +0100

    gcc -Wall -pedantic -Werror -Wextra -std=gnu89 _putchar.c 7-main.c 7-print_chessboard.c -o 7-print_chessboard

---
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+#include "2-strlen.c"
+
+/**
+ *_chessboard - prints the chessboard
+ *@a: array
+ *
+ *Return: NULL
+ */
+void print_chessboard(char (*a)[8])
+{
+	int l;
+	int c;
+
+	for (l = 0; l < 8; l++)
+	{
+		for (c = 0; c < 8; c++)
+		{
+			_putchar(a[l][c]);
+			_putchar('\n');
+		}
+	}
+}
