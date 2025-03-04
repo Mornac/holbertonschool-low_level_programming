1774f9845d0a17830d6ef272ce431bfe809d0cce
Mornac <imornac@gmail.com>
2025-03-04 15:11:59 +0100

    function that returns the value of x raised to the power of y

--- recursion/4-pow_recursion.c
+++ recursion/4-pow_recursion.c
@@ -13,9 +13,14 @@ int _pow_recursion(int x, int y)
 		return (-1);
 	}
 
+	else if (y == 0)
+	{
+		return (1);
+	}
+
 	else
 	{
-		_pow_recursion(x, y);
+		return (x * _pow_recursion(x, y - 1));
 	}
 	return (0);
 }
