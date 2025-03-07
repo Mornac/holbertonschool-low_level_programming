1175f6645f64f5899f70c16f997ade82f37c4620
Mornac <imornac@gmail.com>
2025-03-07 16:25:38 +0100

    program that adds positive numbers

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -11,8 +11,12 @@ int main(int argc, char *argv[])
 {
 	int i, j, sum = 0;
 
-	for (i = 1; i < argc; i++)
+	if (argc == 1)
 	{
+		printf("0\n");
+		return (0);
+	}
+	for (i = 1; i < argc; i++)
 		for (j = 0; argv[i][j] != '\0'; j++)
 		{
 			if ((argv[i][j] < '0') || (argv[i][j] > '9'))
@@ -22,8 +26,9 @@ int main(int argc, char *argv[])
 			}
 
 		}
-		sum += atoi(argv[i]);
-	}
+
+	sum += atoi(argv[i]);
+
 	printf("%d\n", sum);
 	return (0);
 }
