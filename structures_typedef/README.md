8aab4a57d6be5e421d8e87bb55dc09a2fe39342c
Mornac <imornac@gmail.com>
2025-03-14 09:29:55 +0100

    Define a new type struct dog with elements

---
+++ structures_typedef/README.md
@@ -0,0 +1,8 @@
+C - Structures, typedef
+
+0 Define a new type struct dog with specific elements
+1 Function that initialize a variable of type struct dog
+2 Function that prints a struct dog
+3 Define a new type dog_t as a new name for the type struct dog
+4 Function that creates a new dog
+5 function that frees dogs
---
+++ structures_typedef/dog.h
@@ -0,0 +1,29 @@
+#ifndef DOG_H
+#define DOG_H
+ 
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *struct dog - type of structure
+ *@name: name of dog
+ *@age: age of dog
+ *@owner: owner's name
+ *
+ *Description: header for dog
+ */
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+} dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+int _strlen(char *str);
+char *_strcopy(char *dest, char *src);
+
+#endif
