4732b0194c7f3b03fe2e285918210380129cdbd5
Mornac <imornac@gmail.com>
2025-03-14 14:10:18 +0100

    function that frees dogs

---
+++ structures_typedef/5-free_dog.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ *free_dog - function that frees dogs
+ *@d: structure
+ */
+void free_dog(dog_t *d)
+{
+	if (d != NULL)
+	{
+		if (d->name != NULL)
+		{
+			free(d->name);
+		}
+		free(d);
+	}
+}
