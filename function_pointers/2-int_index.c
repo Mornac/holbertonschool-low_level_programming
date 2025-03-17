1f26cb17f77b33cd466021855ff7bc4de59ea941
Mornac <imornac@gmail.com>
2025-03-17 17:15:13 +0100

    function that searches for an integer

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -14,10 +14,13 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	if (size <= 0)
+	if (size < 0)
 		return (-1);
 
-	for (i = 0; i < size; i++)
+	if (array == NULL)
+		return (1); 
+
+	for (i = 0; i <= size; i++)
 	{
 		cmp(array[size]);
 		return (1);
