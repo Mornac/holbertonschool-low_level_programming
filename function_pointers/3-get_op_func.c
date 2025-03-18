9996447ff481067ff1b1b2b26b3458e8c5d988eb
Mornac <imornac@gmail.com>
2025-03-18 11:52:05 +0100

    program that performs simple operations

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include "3-calc.h"
+#include <string.h>
 
 /**
  *get_op_func - selects correct function to perform the operation asked
@@ -22,7 +23,7 @@ int (*get_op_func(char *s))(int, int)
 
 	while (ops[i].op != NULL)
 	{
-		if (*s == *(ops[i].op))
+		if (strcmp(s, ops[i].op) == 0)
 			return (ops[i].f);
 		i++;
 	}
