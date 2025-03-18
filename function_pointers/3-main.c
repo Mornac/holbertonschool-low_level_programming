29c664a609251fda61a8ae986095be8f357d39fd
Mornac <imornac@gmail.com>
2025-03-18 10:54:09 +0100

    program that performs simple operations

---
+++ function_pointers/3-main.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "calc.h"
+
+/**
+ *main - prints the result of the operation
+ *@argc: argument count
+ *@argv: argument vector
+ *Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+	int a, b, result;
+	int (*op)(int, int);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit (98);
+	}
+
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+	op = get_op_func(argv[2]);
+
+
+	if (op == NULL) 
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	if ((*argv[2] == '/' || *argv[2] == '%') && b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	result = op(a, b);
+	printf("%d\n", result);
+	
+	return (0);
+}
---
+++ function_pointers/3-op_functions.c
@@ -0,0 +1,56 @@
+#include "calc.h"
+
+/**
+ *op_add - returns the sum
+ *@a: int
+ *@b: int
+ *Return: Always 0
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ *op_sub - returns the difference
+ *@a: int
+ *@b: int
+ *Return: Always 0
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ *op_mul - returns the product
+ *@a: int
+ *@b: int
+ *Return: Always 0
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ *op_div - returns the result of a division
+ *@a: int
+ *@b: int
+ *Return : Always 0
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ *op_mod - returns the remainder of a division
+ *@a: int
+ *@b: int
+ *Return: Always 0
+ */
+ int op_mod(int a, int b)
+{
+	return (a % b);
+}
