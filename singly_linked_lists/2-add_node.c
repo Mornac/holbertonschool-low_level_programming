a7763f05813d3101ba948f158b1a765c36dd3b91
Mornac <imornac@gmail.com>
2025-03-31 17:05:17 +0200

    adds a new node at the beginning of a list_t list

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -11,6 +11,7 @@
 */
 list_t *add_node(list_t **head, const char *str)
 {
+	unsigned int len;
 	list_t *new_node = (list_t *)malloc(sizeof(list_t));
 
 	if (new_node == NULL)
@@ -24,10 +25,12 @@ list_t *add_node(list_t **head, const char *str)
 		return (NULL);
 	}
 
-	new_node->len = strlen(str);
+	for (len = 0; str[len]; len++)
+
+	new_node->len = len; 
 	new_node->next = *head;
 	*head = new_node;
-
+	
 	return (new_node);
 	free(new_node);
 }
