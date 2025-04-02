63c1cc498ffe5f103e3625cf0d0a8adb9b45461c
Mornac <imornac@gmail.com>
2025-04-02 10:42:40 +0200

    add: delete the return after free(head) in the function that frees a dlistint_t list

--- doubly_linked_lists/4-free_dlistint.c
+++ doubly_linked_lists/4-free_dlistint.c
@@ -9,11 +9,9 @@
  */
 void free_dlistint(dlistint_t *head)
 {
-	if (head)
-	{
-		free_dlistint(head->next);
-		free(head->next);
-	}
+	if (head == NULL)
+		return;
 
+	free_dlistint(head->next);
 	free(head);
 }
