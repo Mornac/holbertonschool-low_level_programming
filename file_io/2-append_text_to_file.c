7c2fc22af50a5e25b42a8d8caed2e6168db906f6
Mornac <imornac@gmail.com>
2025-04-04 11:16:28 +0200

    add: add string in the function that appends text at the end of a file

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <string.h>
 #include "main.h"
 
 /**
@@ -13,22 +14,19 @@
 int append_text_to_file(const char *filename, char *text_content)
 {
 	int fd;
-	ssize_t bytes_written = 0, len = 0;
+	ssize_t bytes_written;
 
 	if (filename == NULL)
 		return (-1);
 
-	fd = open(filename, O_WRONLY, S_IRUSR | S_IWUSR);
+	fd = open(filename, O_WRONLY | O_APPEND);
 	if (fd == -1)
 		return (-1);
 
 	if (text_content != NULL)
 	{
-		while (text_content[len] != '\0')
-			len++;
-
-		bytes_written = write(fd, text_content, len);
-		if (bytes_written == -1 || bytes_written != len)
+		bytes_written = write(fd, text_content, strlen(text_content));
+		if (bytes_written == -1)
 		{
 			close(fd);
 			return (-1);
