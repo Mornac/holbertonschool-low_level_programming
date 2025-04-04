18edd38b6723d66b936ab5994e98b4438fceebbd
Mornac <imornac@gmail.com>
2025-04-04 15:33:10 +0200

    program that copies the content of a file to another file

---
+++ file_io/3-cp.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <string.h>
+#include "main.h"
+
+#define BUFFER_SIZE 1024
+/**
+ *main - copies the content of a file to another file
+ *Return: exit errors
+ */
+int main(int argc, char *argv[])
+{
+	int fptr1, fptr2;
+	char buffer[BUFFER_SIZE];
+	ssize_t bytes_read;
+
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+
+	fptr1 = open(argv[1], O_RDONLY);
+	if (fptr1 == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file file_from\n");
+		exit(98);
+	}
+
+	fptr2 = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+
+	if (fptr2 == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to file_to\n");
+		exit(99);
+	}
+
+	while ((bytes_read  = read(fptr1, buffer, BUFFER_SIZE)) > 0)
+	{
+		write(fptr2, buffer, bytes_read);
+	}
+
+	if (bytes_read == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file file_from\n");
+		exit(98);
+	}
+
+	if (close(fptr1) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fptr1);
+		exit(100);
+	}
+
+	if (close(fptr2) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fptr2);
+		exit(100);
+	}
+	return (0);
+}
