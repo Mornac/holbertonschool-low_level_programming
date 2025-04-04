e676a181c6498fff3c7bb7263876d3dca57b1869
Mornac <imornac@gmail.com>
2025-04-04 16:33:36 +0200

    correct output errors in the program that copies the content of a file to another file

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -39,23 +39,22 @@ int main(int argc, char *argv[])
 
 	while ((bytes_read  = read(fptr1, buffer, BUFFER_SIZE)) > 0)
 	{
+		if (write(fptr2, buffer, bytes_read) == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+			close(fptr1);
+			close(fptr2);
+			exit(99);
+		}
+		
 		if (bytes_read == -1)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 			close(fptr1);
 			close(fptr2);
 			exit(98);
-			
-			if (write(fptr2, buffer, bytes_read) == -1)
-			{
-				dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-				close(fptr1);
-				close(fptr2);
-				exit(99);
-			}
 		}
 	}
-
 	if (close(fptr1) == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close file %s\n", argv[1]);
